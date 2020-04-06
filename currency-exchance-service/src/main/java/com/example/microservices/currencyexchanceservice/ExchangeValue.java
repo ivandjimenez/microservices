68567bcc62818047e43645701768b86f95af15b2
Ivan Jimenez <ivan.jimenez0107@gmail.com>
2020-04-06 15:44:02 -0500

    Section 4: Step 18 to Step 22 Currency Conversion Service

---
+++ currency-exchance-service/src/main/java/com/example/microservices/currencyexchanceservice/ExchangeValue.java
@@ -0,0 +1,44 @@
+package com.example.microservices.currencyexchanceservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class ExchangeValue {
+
+	@Id
+	private long id;
+	@Column(name="currency_from")
+	private String from;
+	@Column(name="currency_to")
+	private String to;
+	private BigDecimal conversionMultiple;
+	public ExchangeValue() {
+		// TODO Auto-generated constructor stub
+	}
+	public ExchangeValue(long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+	
+}
