e8d903f74bcf4dc8bc822eb071e514eb8eeaa435
Ivan Jimenez <ivan.jimenez0107@gmail.com>
2020-04-14 14:32:10 -0500

    Section 4 Micro services with Spring Cloud
    Step 35 to 44

--- limits-service/src/main/java/com/example/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/example/microservices/limitsservice/LimitsConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -17,4 +18,17 @@ public class LimitsConfigurationController {
 		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
 		
 	}
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration RetrieveConfiguration() {
+		throw new RuntimeException("Not available");	
+	}
+
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(9,999);
+	
+	}
+
 }
+
